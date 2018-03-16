9e0da38246d7e6b461afc1ee84b2205b55c58d3e
viviennelee <viviennelee@gmail.com>
2018-03-15 22:44:25 -0700

    Fix

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import opentok from './opentok';
+import OTSession from './OTSession';
+import OTPublisher from './OTPublisher';
+import OTStreams from './OTStreams';
+import OTSubscriber from './OTSubscriber';
+import createSession from './createSession';
+import preloadScript from './preloadScript';
+
+export default {
+  opentok,
+  OTSession,
+  OTPublisher,
+  OTStreams,
+  OTSubscriber,
+  createSession,
+  preloadScript,
+};
+
+export {
+  opentok,
+  OTSession,
+  OTPublisher,
+  OTStreams,
+  OTSubscriber,
+  createSession,
+  preloadScript,
+};
